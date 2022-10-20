4cf334637f269c616d11e8d3f5b7633b7bca0414
NumberFortyEight <pavel.lakhtin.work@gmail.com>
2022-10-21 01:00:27 +0500

    init commit

---
+++ src/main/java/com/fortyeight/tool/relationalvisualizer/RelationalVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.fortyeight.tool.relationalvisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalVisualizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationalVisualizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fortyeight/tool/relationalvisualizer/RelationalVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortyeight.tool.relationalvisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalVisualizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
